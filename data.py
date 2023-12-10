bfd8003059679fa105dcd6179ec1a4fa3bf53579
Satyam Ghodasara <sghodas@gmail.com>
2023-12-10 13:29:19 -0500

    Add tensor sorting transform

--- data.py
+++ data.py
@@ -24,9 +24,9 @@ class PixelDataModule(pl.LightningDataModule):
                 T.ToTensor(),
                 T.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
                 T.Resize((320, 320), antialias=True),
-                T.RandomRotation(15),
-                T.RandomResizedCrop((290, 290), (0.8, 1.2), antialias=True),
-                # T.Lambda(PixelDataModule._sort_values),
+                # T.RandomRotation(15),
+                # T.RandomResizedCrop((290, 290), (0.8, 1.2), antialias=True),
+                T.Lambda(PixelDataModule._sort_values),
                 # T.Lambda(PixelDataModule._round_values),
             ]
         )
@@ -36,7 +36,7 @@ class PixelDataModule(pl.LightningDataModule):
                 T.ToTensor(),
                 T.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
                 T.Resize((320, 320), antialias=True),
-                # T.Lambda(PixelDataModule._sort_values),
+                T.Lambda(PixelDataModule._sort_values),
                 # T.Lambda(PixelDataModule._round_values),
             ]
         )
